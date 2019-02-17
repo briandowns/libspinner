aa1f6448349bd673938c39558ff4545995f53c1e
Brian Downs <brian.downs@gmail.com>
2019-02-16 22:45:55 -0700

    fix spinners

--- examples/main.c
+++ examples/main.c
@@ -7,7 +7,7 @@
 int 
 main(int argc, char **argv) 
 {
-	spinner_t *s = spinner_new(31);
+	spinner_t *s = spinner_new(9);
 	s->delay = 100000; 
 	s->prefix = "Running ";
 	s->final_msg = "\nAll done\n";
