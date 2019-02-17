64f343764e7d972764ecd478f078b559d52267b3
Brian Downs <brian.downs@gmail.com>
2019-02-17 09:07:50 -0700

    fix license header

--- spinner.h
+++ spinner.h
@@ -1,7 +1,7 @@
 /*-
  * SPDX-License-Identifier: BSD-3-Clause
  *
- * Copyright (c) 2019 phxsh Authors
+ * Copyright (c) 2019 Brian J. Downs
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
